a24c8add3302345606e4a355edaa9d13c19f2f2e
RodrigoLee56 <sousarodrigo56@outlook.com>
2024-06-03 11:28:03 -0400

    Criando o app da primeira aula do curso

---
+++ app/src/main/java/com/rodrigolee/samsung/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.rodrigolee.samsung
+
+import android.os.Bundle
+import android.util.Log
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
+import android.widget.Toast
+import com.rodrigolee.samsung.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+        var button = findViewById<Button>(R.id.button)
+        Log.d("oceanbrasil", "O texto do botao é:"+button.text.toString())
+        Toast.makeText(this, "Clicou", Toast.LENGTH_LONG).show()
+
+    }
+}
